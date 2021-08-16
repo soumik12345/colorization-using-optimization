cb50bcb10c53e119e4fce7cb972649ca22b75bf7
soumik12345 <19soumik.rakshit96@gmail.com>
2021-08-16 13:14:19 +0530

    Updated iterative colorizer

--- colorization/iterative_colorizer/iterative_colorizer.py
+++ colorization/iterative_colorizer/iterative_colorizer.py
@@ -2,6 +2,7 @@ import cv2
 import scipy
 import colorsys
 import numpy as np
+from tqdm import tqdm
 from typing import Tuple
 from skimage.io import imread
 from matplotlib import pyplot as plt
@@ -46,62 +47,64 @@ class IterativeColorizer:
         """Combine 3 channels of YUV to a RGB photo: n x n x 3 array"""
         result_rgb = [colorsys.yiq_to_rgb(
             channel_y[i], channel_u[i], channel_v[i]
-        ) for i in range(len(self.ansY))]
+        ) for i in range(len(self.result_y))]
         result_rgb = np.array(result_rgb)
-        image_rgb = np.zeros(self.pic_yuv.shape)
-        image_rgb[:, :, 0] = result_rgb[:, 0].reshape(self.pic_rows, self.pic_cols, order='F')
-        image_rgb[:, :, 1] = result_rgb[:, 1].reshape(self.pic_rows, self.pic_cols, order='F')
-        image_rgb[:, :, 2] = result_rgb[:, 2].reshape(self.pic_rows, self.pic_cols, order='F')
+        image_rgb = np.zeros(self.image_yuv.shape)
+        image_rgb[:, :, 0] = result_rgb[:, 0].reshape(self.image_rows, self.image_cols, order='F')
+        image_rgb[:, :, 1] = result_rgb[:, 1].reshape(self.image_rows, self.image_cols, order='F')
+        image_rgb[:, :, 2] = result_rgb[:, 2].reshape(self.picimage_rows_rows, self.image_cols, order='F')
         return image_rgb
     
     def colorize(self) -> np.ndarray:
-        (self.pic_rows, self.pic_cols, _) = self.image_original.shape
-        pic_size = self.pic_rows * self.pic_cols
-        channel_Y,_,_ = colorsys.rgb_to_yiq(
+        (self.image_rows, self.image_cols, _) = self.image_original.shape
+        image_size = self.image_rows * self.image_cols
+        channel_Y, _, _ = colorsys.rgb_to_yiq(
             self.image_original[:, :, 0],
             self.image_original[:, :, 1],
             self.image_original[:, :, 2]
         )
-        _,channel_U,channel_V = colorsys.rgb_to_yiq(
+        _, channel_U, channel_V = colorsys.rgb_to_yiq(
             self.image_clues[:, :, 0],
             self.image_clues[:, :, 1],
             self.image_clues[:, :, 2]
         )
         map_colored = (abs(channel_U) + abs(channel_V)) > 0.0001
-        self.pic_yuv = np.dstack((channel_Y, channel_U, channel_V))
+        self.image_yuv = np.dstack((channel_Y, channel_U, channel_V))
         weight_data = []
         # num_pixel_bw = 0
         wd_width = 1
-        for c in range(self.pic_cols):
-            for r in range(self.pic_rows):
-                res = []
-                w = WindowNeighbor(wd_width, (r, c), self.pic_yuv)
+        progress_bar = tqdm(range(self.image_cols))
+        progress_bar.set_description('Finding neighbouring pixels...')
+        for c in progress_bar:
+            for r in range(self.image_rows):
+                window_neighbour = WindowNeighbor(wd_width, (r, c), self.image_yuv)
                 if not map_colored[r,c]:
-                    weights = affinity_a(w)
+                    weights = affinity_a(window_neighbour)
                     for e in weights:
-                        weight_data.append([w.center,(e[0],e[1]), e[2]])
-                weight_data.append([w.center, (w.center[0],w.center[1]), 1.])
-        sp_idx_rc_data = [
+                        weight_data.append([window_neighbour.center, (e[0], e[1]), e[2]])
+                weight_data.append([
+                    window_neighbour.center, (window_neighbour.center[0], window_neighbour.center[1]), 1.])
+        sparse_index_data = [
             [
-                to_seq(e[0][0], e[0][1], self.pic_rows),
-                to_seq(e[1][0], e[1][1], self.pic_rows), e[2]
+                to_seq(e[0][0], e[0][1], self.image_rows),
+                to_seq(e[1][0], e[1][1], self.image_rows), e[2]
             ] for e in weight_data
         ]
-        sp_idx_rc = np.array(sp_idx_rc_data, dtype=np.integer)[:, 0:2]
-        sp_data = np.array(sp_idx_rc_data, dtype=np.float64)[:, 2]
-        matA = scipy.sparse.csr_matrix(
-            (sp_data, (sp_idx_rc[:,0], sp_idx_rc[:,1])),
-            shape=(pic_size, pic_size)
+        sparse_index_row_col = np.array(sparse_index_data, dtype=np.integer)[:, 0:2]
+        sparse_data = np.array(sparse_index_data, dtype=np.float64)[:, 2]
+        weight_matrix = scipy.sparse.csr_matrix(
+            (sparse_data, (sparse_index_row_col[:,0], sparse_index_row_col[:,1])),
+            shape=(image_size, image_size)
         )
-        b_u = np.zeros(pic_size)
-        b_v = np.zeros(pic_size)
-        idx_colored = np.nonzero(map_colored.reshape(pic_size, order='F'))
-        pic_u_flat = self.pic_yuv[:,:,1].reshape(pic_size, order='F')
+        b_u = np.zeros(image_size)
+        b_v = np.zeros(image_size)
+        idx_colored = np.nonzero(map_colored.reshape(image_size, order='F'))
+        pic_u_flat = self.image_yuv[:,:,1].reshape(image_size, order='F')
         b_u[idx_colored] = pic_u_flat[idx_colored]
-        pic_v_flat = self.pic_yuv[:,:,2].reshape(pic_size, order='F')
+        pic_v_flat = self.image_yuv[:,:,2].reshape(image_size, order='F')
         b_v[idx_colored] = pic_v_flat[idx_colored]
-        self.ansY = self.pic_yuv[:,:,0].reshape(pic_size, order='F')
-        ansU = spsolve(matA, b_u)
-        ansV = spsolve(matA, b_v)
-        result = self.yuv_channels_to_rgb(self.ansY, ansU, ansV)
+        self.result_y = self.image_yuv[:,:,0].reshape(image_size, order='F')
+        self.result_u = spsolve(weight_matrix, b_u)
+        self.result_v = spsolve(weight_matrix, b_v)
+        result = self.yuv_channels_to_rgb(self.result_y, self.result_u, self.result_v)
         return result
